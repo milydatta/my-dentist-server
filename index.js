f6aa09add256fd0ce21c39d26f567d91100bb3b2
mily datta <dmily270@gmail.com>
2021-06-16 20:22:41 +0600

    some changes

--- index.js
+++ index.js
@@ -16,14 +16,14 @@ app.use(express.static('doctors'));
 app.use(fileUpload());
 
 // Add Access Control Allow Origin headers
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-  });
+// app.use((req, res, next) => {
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     res.header(
+//       "Access-Control-Allow-Headers",
+//       "Origin, X-Requested-With, Content-Type, Accept"
+//     );
+//     next();
+//   });
 
 const port = 5050;
 
