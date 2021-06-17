77b2f1e37b6b78f23fe24d62dd8d2e023a47db71
mily datta <dmily270@gmail.com>
2021-06-17 19:28:41 +0600

    some update

--- index.js
+++ index.js
@@ -15,16 +15,6 @@ app.use(cors());
 app.use(express.static('doctors'));
 app.use(fileUpload());
 
-// Add Access Control Allow Origin headers
-// app.use((req, res, next) => {
-//     res.setHeader("Access-Control-Allow-Origin", "*");
-//     res.header(
-//       "Access-Control-Allow-Headers",
-//       "Origin, X-Requested-With, Content-Type, Accept"
-//     );
-//     next();
-//   });
-
 const port = 5050;
 
 app.get('/', (req, res) => {
